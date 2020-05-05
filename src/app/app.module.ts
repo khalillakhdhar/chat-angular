c6a3c5c9da540aeee2f3e5d01358f22a8b5b002c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-05-05 22:16:44 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 import { FormsModule } from '@angular/forms';
 
-import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -13,7 +15,7 @@ import { ReadComponent } from './read/read.component';
 import { SendmsgComponent } from './sendmsg/sendmsg.component';
 import { ReadmsgComponent } from './readmsg/readmsg.component';
 import { ReadtopicComponent } from './readtopic/readtopic.component';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
